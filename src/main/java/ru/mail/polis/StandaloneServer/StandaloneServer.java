067b08395caaa5b2b0d8ac695004ecc7464de526
gorbatov <gorbatov.eg@gmail.com>
2018-09-29 13:31:19 +0300

    Stage-1. Handling all entity-requests by entity method.

--- src/main/java/ru/mail/polis/StandaloneServer/StandaloneServer.java
+++ src/main/java/ru/mail/polis/StandaloneServer/StandaloneServer.java
@@ -9,7 +9,8 @@ import java.util.NoSuchElementException;
 
 public class StandaloneServer extends HttpServer implements KVService {
     private final String ID_PARAM = "id=";
-    private final String URL = "/v0/entity";
+    private final String ENTITY_PATH = "/v0/entity";
+    private final String STATUS_PATH = "/v0/status";
     private final byte[] EMPTY_BODY = new byte[0];
 
     private final KVDao dao;
@@ -19,7 +20,7 @@ public class StandaloneServer extends HttpServer implements KVService {
         this.dao = dao;
     }
 
-    @Path("/v0/status")
+    @Path(STATUS_PATH)
     public void status(Request request, HttpSession session) throws IOException {
         if (request.getMethod() == Request.METHOD_GET) {
             session.sendResponse(Response.ok(Response.EMPTY));
@@ -28,10 +29,10 @@ public class StandaloneServer extends HttpServer implements KVService {
         }
     }
 
-    @Override
-    public void handleDefault(Request request, HttpSession session) throws IOException {
+    @Path(ENTITY_PATH)
+    public void entity(Request request, HttpSession session) throws IOException {
         try {
-            if (request.getPath().equals(URL) && !request.getParameter(ID_PARAM).isEmpty()) {
+            if (!request.getParameter(ID_PARAM).isEmpty()) {
                 switch (request.getMethod()) {
                     case Request.METHOD_PUT: {
                         dao.upsert(request.getParameter(ID_PARAM).getBytes(), request.getBody());
@@ -53,15 +54,20 @@ public class StandaloneServer extends HttpServer implements KVService {
                     }
 
                 }
-            } else{
+            } else {
                 session.sendError(Response.BAD_REQUEST, null);
             }
-        } catch (NoSuchElementException nSEE) {
+        }catch (NoSuchElementException nSEE) {
             nSEE.printStackTrace();
             session.sendError(Response.NOT_FOUND, null);
         } catch (Exception e) {
             e.printStackTrace();
             session.sendError(Response.INTERNAL_ERROR, null);
         }
+    }
+
+    @Override
+    public void handleDefault(Request request, HttpSession session) throws IOException {
+        session.sendError(Response.NOT_FOUND, null);
     }
 }
